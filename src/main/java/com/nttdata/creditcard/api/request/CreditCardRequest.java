d5adbfd4fcd94305d4db9ec50e72c6db6d820e5f
Eduardo Orrego <corregoa@everis.com>
2024-03-17 18:54:48 -0500

    Refactoring Entity Collection - Functional Requeriments

--- src/main/java/com/nttdata/creditcard/api/request/CreditCardRequest.java
+++ src/main/java/com/nttdata/creditcard/api/request/CreditCardRequest.java
@@ -1,7 +1,6 @@
 package com.nttdata.creditcard.api.request;
 
 import com.nttdata.creditcard.enums.DebitCardStatusEnum;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
